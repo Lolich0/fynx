80131a0b308cd3bf5924971e6417248315629d06
Glitch (zeriodev-music-bot) <none>
2020-05-02 08:45:07 +0000

    🤑🗻 Checkpoint ./commands/help.js:8128825/713

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+
+let embed = new Discord.RichEmbed()
+.setTitle("TEST CMD")
+.setDescription(`OKOKOK`)
+.addField('TEST EST');
+
+message.channel.send({Embed});
+
+
+
+
+module.exports.config = {
+    name: "help",
+    aliases: ["hh"]
+
+};

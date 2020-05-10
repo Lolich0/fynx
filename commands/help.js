eaaa49c0cbdb20857671be15773b87608f25c891
Glitch (zeriodev-music-bot) <none>
2020-05-10 23:24:44 +0000

    🌄🐟 Checkpoint ./commands/help.js:8128825/214

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message) => {
 
   
     message.channel.send({embed: {color: embedSuccess, title: `Music Commands` ,
-                                  description: `\n\n**\`1play [URL/song Title]\`** - Plays The First Song From Youtube\n\n \`1skip\` - skips the currents song \n\n ` }})
+                                  description: `\n\n**\`1play [URL/song Title]\`** - Plays The First Song From Youtube\n\n \`1skip\` - skips the currents song \n\n **\`1stop\`** Stops the music and leave the voice channel. \n\n **\`1queue\`** Shows the music queue. \n\n **\`1np\`** Shows what is playing now. \n\n **\`1pause\`** Stops the song for short time ` }})
 
 
 };

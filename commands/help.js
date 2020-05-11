b00ee20baf968df93212a9b4c6954ee09eaf4ee7
Glitch (zeriodev-music-bot) <none>
2020-05-11 11:44:43 +0000

    📚📺 Checkpoint ./commands/help.js:8155730/1397 ./package.json:8155730/5163

--- commands/help.js
+++ commands/help.js
@@ -5,10 +5,7 @@ const embedFail = "#f30707";
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message) => {
-    const embed = new Discord.MessageEmbed()
-.setDescription(`\n\n My prefix is: \`!\` \n\n**\`play [URL/song Title]\`** - Plays The First Song From Youtube\n\n \`skip\` - skips the currents song \n\n **\`stop\`** - Stops the music and leave the voice channel. \n\n **\`queue\` - ** Shows the music queue. \n\n **\`np\` - ** Shows what is playing now. \n\n **\`pause\` - ** Stops the song for short time \n\n **\`repeat\`** - Repeats the song that is playing now. \n\n **\`resume\`** - Continue playing from when songs got paused. \n\n **\`cq\`** - Clears all the queue. \n\n **\`volume\`** - Changes the volume of the songs. \n\n **\`shuffle\`** - Plays a song from queue randomly. `)
-.setFooter('Requested by' + message.author.tag)
-message.channel.send(embed)
+message.channel.send({embed: { description: `\n\n My prefix is: \`!\` \n\n**\`play [URL/song Title]\`** - Plays The First Song From Youtube\n\n \`skip\` - skips the currents song \n\n **\`stop\`** - Stops the music and leave the voice channel. \n\n **\`queue\` - ** Shows the music queue. \n\n **\`np\` - ** Shows what is playing now. \n\n **\`pause\` - ** Stops the song for short time \n\n **\`repeat\`** - Repeats the song that is playing now. \n\n **\`resume\`** - Continue playing from when songs got paused. \n\n **\`cq\`** - Clears all the queue. \n\n **\`volume\`** - Changes the volume of the songs. \n\n **\`shuffle\`** - Plays a song from queue randomly. `}})
 
 
 };

e33f9714cb34274b5a85a51e18efabb88674fd6b
Glitch (ultra-bot-) <none>
2020-07-20 10:41:28 +0000

    🎰⛰ Checkpoint ./commands/yardım.js:2344545/12352

--- commands/yardım.js
+++ commands/yardım.js
@@ -1,28 +1,56 @@
-const { MessageEmbed } = require("discord.js");
-const prefix = "!";
-
-module.exports.run = async (client, message, args) => {
-    let embed = new MessageEmbed()
-    .setTitle(`Harmony - Yardım`)
-    .addField(prefix + 'yardım', `Bütün komutları ve açıklamaları gösterir.`)
-    .addField(prefix + 'oynat <Şarkı İsmi>', `Belirtilen isimli şarkıları arar ve bulunduğunuz odaya bağlanıp müziği oynatır.`, true)
-    .addField(prefix + 'durdur', `Harmony müzik oynatmayı durdurur ve ses kanalından ayrılır.`)    
-    .addField(prefix + 'atla', `Oynatılan olan müziği atlar. Kuyrukta müzik var ise müzikler sırayla oynatılır.`, true)
-    .addField(prefix + 'duraklat', `Oynatılan olan müziği duraklatır.`)
-    .addField(prefix + 'devam', `Duraklatılan müziği devam ettirir.`, true)
-    .addField(prefix + 'karıştır', `Müzik kuyruğundaki müzikleri karıştırır.`)
-    .addField(prefix + 'döngü', `Müzik kuyruğundaki müzikleri döngü içerisine alır.`, true)    
-    .addField(prefix + 'çalan', `Oynatılan olan müziği gösterir.`)
-    .addField(prefix + 'kuyruk', `Müzik kuyruğunu gösterir.`, true)
-    .addField(prefix + 'kuyruğu-temizle', `Müzik kuyruğunu temizler.`)
-    .addField('Harmony Bot', `\n-[Botu Davet Et!](https://discordapp.com/oauth2/authorize?client_id=511593657711722523&scope=bot&permissions=8)\n-[Destek Sunucusu](https://discord.com/app)`)
-    .setColor("#F8AA2A")
-    .setFooter(`${message.author.username} Tarafından Kullanıldı.`)
-    .setTimestamp();
-    return message.channel.send(embed);
-  };
+const Discord = require('discord.js'); 
+ 
+module.exports.run = async(client, message, args) => {
+ //OSKOBS
+  let sayfalar = ['İlk Sayfa', '2.Sayfa', 'OOO 3.Yardım Sayfası']; 
+  let page = 1; 
+ 
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Harmony Yardım Menüsü") 
+    .setColor("BLUE")
+    .setFooter(`Sayfa ${page} - ${sayfalar.length}`) 
+    .setDescription(sayfalar[page-1])
+ 
+  message.channel.send(embed).then(msg => { 
+   
+    msg.react('⏪').then( r => { 
+      msg.react('⏩') 
+     
+      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+      const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
+     
+      const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+      const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
+     
+      
+      backwards.on('collect', r => { 
+        if (page === 1) return; 
+        page--; 
+        embed.setTitle("Yardım Menüsü")
+        embed.setDescription(sayfalar[page-1]); 
+        embed.setFooter(`Sayfa ${page} - ${sayfalar.length}`);
+        embed.setColor("RED") 
+        msg.edit(embed) 
+      })
+ //OSKOBS    
+      forwards.on('collect', r => { 
+        if (page === sayfalar.length) return; 
+        page++; 
+        embed.setTitle("Sayfalı Yardım Menüsü")
+        embed.setDescription(sayfalar[page-1]); 
+        embed.setFooter(`Sayfa ${page} - ${sayfalar.length}`);
+        embed.setColor("BLUE") 
+        msg.edit(embed) 
+      })
+   
+    })
+ 
+  })
+ 
+}
 
 module.exports.config = {
-    name: "yardım",
-    aliases: ["y"]
+ name: 'yardım',
+  aliases: ["sayfalıyardım","pagehelp","h","help"],
+
 };

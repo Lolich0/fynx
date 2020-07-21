16f72c6c600fb1654326fcf3d392d11c5c37e22a
Glitch (linen-believed-fiber) <none>
2020-07-21 12:03:23 +0000

    🎍🚠 Checkpoint ./commands/istatistik.js:2344545/213

--- commands/istatistik.js
+++ commands/istatistik.js
@@ -10,15 +10,15 @@ exports.run = async (client, message, args) => {
     .setColor("RANDOM")
     .setTimestamp()
     .setFooter("Harmony © 2020", client.user.avatarURL())
-    .addField("» **Botun Sahibi**", "<@327064201245753344>")
-    .addField("» **Gecikme süreleri**","Mesaj Gecikmesi: {ping1} ms \nBot Gecikmesi: {ping2} ms"
+    .addField("<a:coder:734885134800519288>  | **Botun Sahibi**", "<@327064201245753344>")
+    .addField("<:ping:735102803340558336>  | **Gecikme süreleri**","Mesaj Gecikmesi: {ping1} ms \nBot Gecikmesi: {ping2} ms"
         .replace("{ping1}", new Date().getTime() - message.createdTimestamp)
         .replace("{ping2}", client.ws.ping),true)
-    .addField("» **Bellek kullanımı**",(process.memoryUsage().heapUsed / 1024 / 512).toFixed(2) + " MB",true)
-    .addField("» **Çalışma süresi**", seksizaman, true)
-    .addField("» **Discord.JS sürüm**", "v" + Discord.version, true)
-    .addField("» **Node.JS sürüm**", `${process.version}`, true)
-    .addField("» **Müzik Çalınan Sunucu Sayısı**", client.voice.connections.size, true)
+    .addField("<:bellek:735103897449922610>  | **Bellek kullanımı**",(process.memoryUsage().heapUsed / 1024 / 512).toFixed(2) + " MB",true)
+    .addField("<:uptime:735102804468957284>  | **Çalışma süresi**", seksizaman, true)
+    .addField("<:discordjs:735102801449058364>  | **Discord.JS sürüm**", "v" + Discord.version, true)
+    .addField("<:nodejs:735102802614943826>  | **Node.JS sürüm**", `${process.version}`, true)
+    .addField(":microphone:  | **Müzik Çalınan Sunucu Sayısı**", client.voice.connections.size, true)
   return message.channel.send(istatistikler);
 };
 

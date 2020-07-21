b69fc72729a9c4b973203f68e6ebee85ebd4c643
Glitch (linen-believed-fiber) <none>
2020-07-21 13:23:17 +0000

    🏑👟 Checkpoint ./index.js:2344545/797

--- index.js
+++ index.js
@@ -83,3 +83,14 @@ dm.send(botdm)
 if(msg.channel.bot) return;
 });
 
+client.on('voiceStateUpdate', async (___, newState) => {
+
+  if (
+    newState.member.user.bot &&
+    newState.channelID &&
+    newState.member.user.id == client.user.id &&
+    !newState.selfDeaf
+  ) {
+    newState.setSelfDeaf(true);
+  }
+});

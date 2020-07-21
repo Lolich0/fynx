14f363f16106f1625966f3410d02b4d6eeee5f3d
Glitch (linen-believed-fiber) <none>
2020-07-21 20:36:25 +0000

    👨‍👨‍👦👧 Checkpoint ./commands/yardım.js:2344545/5719

--- commands/yardım.js
+++ commands/yardım.js
@@ -1,60 +1,55 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js'); 
  
-module.exports.run = (client, message, args) => {
+exports.run = async(client, message, args) => {
  
-  let pages = [
-              "<a:ucankalpler:735102535974780968>  Yardım menüsünü görmeden önce Harmony'i kullandığınız için sizlere teşekkür ederiz.\n\n<a:kalp:734885134251327540>  Umarım botumuzu beğenerek kullanıyorsunuzdur.\n\n<a:coder:734885134800519288>  Sizler sayesinde botumuz daha iyi yerlere geliyor.\n\n<:developer:734885133684965428>  Eksiklerimiz veya hatalarımızı `!tavsiye` komutu ile bizimle paylaşabilirsiniz.",
-              '**`!yardım`**\n<a:secenek:733987076718198864>  Bütün komutları ve açıklamaları gösterir. \n\n **`!oynat <Şarkı İsmi>`**\n<a:secenek:733987076718198864>  Belirtilen isimli şarkıları arar ve bulunduğunuz odaya bağlanıp müziği oynatır. \n\n **`!durdur`**\n<a:secenek:733987076718198864>  Harmony müzik oynatmayı durdurur ve ses kanalından ayrılır.\n\n  **`!atla`**\n<a:secenek:733987076718198864>  Oynatılan olan müziği atlar. Kuyrukta müzik var ise müzikler sırayla oynatılır. \n\n **`!duraklat`**\n<a:secenek:733987076718198864>  Oynatılan olan müziği duraklatır. \n\n **`!devam`**\n<a:secenek:733987076718198864>  Duraklatılan müziği devam ettirir.',
-              '**`!karıştır`**\n<a:secenek:733987076718198864>  Müzik kuyruğundaki müzikleri karıştırır. \n\n **`!döngü`**\n<a:secenek:733987076718198864>  Müzik kuyruğundaki müzikleri döngü içerisine alır. \n\n **`!çalan`**\n<a:secenek:733987076718198864>  Oynatılan olan müziği gösterir.\n\n  **`!kuyruk`**\n<a:secenek:733987076718198864>  Müzik kuyruğunu gösterir. \n\n **`!kuyruğu-temizle`**\n<a:secenek:733987076718198864>  Müzik kuyruğunu temizler.\n\n **`!ses <0/100>`**\n<a:secenek:733987076718198864>  Müzik kuyruğunu temizler.(BAKIMDA)',
-              '<:bot:734888733853089903>  | [Harmony`i Sunucunuza Ekleyin!](https://discordapp.com/oauth2/authorize?client_id=511593657711722523&scope=bot&permissions=8)\n\n<:discord:734888645722636441>  | [Harmony Destek Sunucusu](https://discord.com/app)\n\n<a:twitter:734888743277953077>  | [Harmony Twitter Hesabı](https://twitter.com/harmonybot)\n\n<:instagram:734889120848937011>  | [Harmony Yapımcı İnstagram Hesabı](https://instagram.com/m_arda_dusova)',
-              ];
-  let page = 1;
+ const Embed = new Discord.RichEmbed()
+ .setTimestamp()
+ .setAuthor("Harmony Blocker", client.user.avatarURL)
+.setColor("BLUE")
+.setTitle("YT-at BOT")
+ .setURL("https://discordapp.com/oauth2/authorize?client_id=647386467844227074&scope=bot&permissions=8")
+ .setDescription(`
+
+**?yardım**, ile yardım alabilirsiniz.
+Örnek komut kullanımı: \`!küfüraç\`
+Botu davet etmek için: \`!davet\`
+`)
+ .addField("!komutlar (13)", `
+herkesin kullanabileceği standart komutlar;
+\`botbilgi\`,\`davet\`,\`oyver\`,\`gold\`,\`webpanel\`,\`sunucu-pp\`,\`profil\`,\`kredim\`,\`p-market\`,\`p-menü\`,\`puanım\`,\`ayarlar\`
+`)
+ .addField("!eğlence (9)", `
+herkesin kullanabileceği eğlence komutları;
+\`kralol\`,\`maymunol\`,\`yılanol\`,\`adamol\`,\`kediol\`,\`aşk\`,\`token\`,\`tokat\`
+`)
  
-  const embed = new Discord.MessageEmbed()
-    .setColor('RANDOM')
-    .setThumbnail('https://cdn.discordapp.com/attachments/703582257570644029/734646429833756732/HarmonyMusic.png')
-    .setAuthor(`Harmony Yardım Menüsü`, client.user.avatarURL)
-    .setFooter(`Sayfa ${page} / ${pages.length}`)
-    .setDescription(pages[page-1])
-  message.channel.send(embed).then(msg => {
+ .addField("!moderasyon (13)", `
+yetkililer için moderasyon komutları;
+\`küfür\`,\`reklam\`,\`link engel\`,\`reklam kick\`,\`reklam isim ban\`,\`oto rol\`,\`sayaç\`,\`ban koruma sistemi\`,\`duyuru\`,\`oto cevap\`,\`mute sistemi\`,\`anti raid bot sistemi\`
+`)
  
-  msg.react('⬅')
-  .then(r => {
-    msg.react('➡')
+ .addField("!moderasyon 2 (12)", `
+yetkililer için moderasyon komutlarının 2. bölümü;
+\`sil\`,\`reklam taraması\`,\`resimli hg bb\`,\`sunucu tanıt\`,\`oto bot silici\`,\`ultra sohbet temizleyici\`,\`yavaş mod\`,\`duyuru\`,\`tag sistemi\`,\`rol sistemi\`,\`geçici oda sistemi\`,\`kayıt sistemi\`
+`)
  
-      //Filter
-      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅' && user.id === message.author.id;
-      const forwardsFilter = (reaction, user) => reaction.emoji.name === '➡' && user.id === message.author.id;
- 
-      const backwards = msg.createReactionCollector(backwardsFilter, { time: 100000 });
-      const forwards = msg.createReactionCollector(forwardsFilter, { time: 100000 });
- 
-      forwards.on('collect', r => {
-        if(page === pages.length) return;
-        page++;
-        embed.setDescription(pages[page-1]);
-        embed.setColor('RANDOM')
-        embed.setFooter(`Sayfa ${page} / ${pages.length}`)
-        msg.edit(embed)
-      })
-      backwards.on('collect', r => {
-        if(page === 1) return;
-        page--;
-        embed.setColor('RANDOM')
-        embed.setDescription(pages[page-1]);
-        embed.setFooter(`Sayfa ${page} / ${pages.length}`)
-        msg.edit(embed)
-      })
- 
-    })
-  })
+ .addField("!koruma (5)", `
+Sunucunuz İçin Koruma Sistemleri.
+\`sağ tık ban koruması\`,\`kanal silme koruması\`,\`rol silme koruması\`,\`sunucuya bot sokma koruması\`,\`güvenlik odası\`
+`)
+ .setFooter("Harmony Blocker", client.user.avatarURL)
+ message.channel.send(Embed)
+}
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["y","help","h","help"],
+  permLevel: 0
 };
- 
- 
-module.exports.config = {
-name: 'yardım',
-aliases: ["help", "y", "h"],
-    permlevel: 0
+
+module.exports.help = {
+  name: 'y',
+  description: 'Yardım Menüsünü Gösterir.',
+  usage: 'y'
 };
- 

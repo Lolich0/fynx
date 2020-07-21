e44083eb675415c933cf018573e0bf8ae49ab443
Glitch (linen-believed-fiber) <none>
2020-07-21 12:25:36 +0000

    🛎😂 Checkpoint ./commands/yardım.js:2344545/770 ./commands/atla.js:2344545/96 ./commands/devam.js:2344545/96

--- commands/devam.js
+++ commands/devam.js
@@ -5,13 +5,13 @@ const embedFail = "#f30707";
 
 module.exports.run = async (client, message) => {
 
-    if(!message.member.voice.channel) return message.channel.send({embed: {color: embedFail, description: `Duraklatılan bir müziği devam ettirebilmek için bir ses kanalında olmanız gerekmektedir!` }})
+    if(!message.member.voice.channel) return message.channel.send({embed: {color: embedFail, description: `<a:yanlis:734892943332212764>  | Duraklatılan bir müziği devam ettirebilmek için bir ses kanalında olmanız gerekmektedir!` }})
 
     const song = await client.player.resume(message.guild.id);
 
-    if(!song) return message.channel.send({embed: {color: embedFail, description: `Şu anda hiçbir müzik çalmamaktadır!` }})
+    if(!song) return message.channel.send({embed: {color: embedFail, description: `<a:yanlis:734892943332212764>  | Şu anda hiçbir müzik çalmamaktadır!` }})
 
-    message.channel.send({embed: {color: embedSuccess, description: `\`${song.name}\` adlı müzik devam ettiriliyor.` }})
+    message.channel.send({embed: {color: embedSuccess, description: `<a:tik:734892939737694239>  | \`${song.name}\` adlı müzik devam ettiriliyor.` }})
 
 };
 

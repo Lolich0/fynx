5fba3be8f03b2537f364c0c899a0dc76044845d1
Glitch (harmonymuzik) <none>
2020-07-23 13:11:57 +0000

    🎐💒 Checkpoint ./commands/kuyruk.js:2344545/2330

--- commands/kuyruk.js
+++ commands/kuyruk.js
@@ -8,15 +8,14 @@ module.exports.run = async (client, message) => {
     if(!message.member.voice.channel) return message.channel.send({embed: {color: embedFail, description: `<a:yanlis:734892943332212764>  | Kuyruğu görüntüleyebilmek için bir ses kanalında olmanız gerekmektedir!` }})
 
     const queue = client.player.getQueue(message.guild.id);
-      const thm = client.player.isPlaying.Thumbnail(message.guild.id);
 
 
     if(!queue) return message.channel.send({embed: {color: embedFail, description: `<a:yanlis:734892943332212764>  | Şu anda hiçbir müzik çalmamaktadır!` }})
 
     let q = queue.songs.map((song, i) => {
-        return `${i === 0 ? '<a:calan:735111831550427166>  | Şu Anda Çalınan Müzik' : `${i+1}`} \n**Müzik:** \`${song.name}\` \n**Kanal:** \`${song.author}\`\n**Süre:** \`${song.duration}\``
+        return `${i === 0 ? '<a:calan:735111831550427166>  | Şu Anda Çalınan Müzik' : `${i+1}`} \n**Müzik:** \`${song.name}\` \n**Kanal:** \`${song.author}\`\n**Tarafından İstendi:** \`${song.requestedBy}\``
     }).join('\n');  
-       message.channel.send({embed: {color: embedSuccess, description: `${q}`, thumbnail: `${thm}` }})
+       message.channel.send({embed: {color: embedSuccess, description: `${q}`}})
 };
 
   

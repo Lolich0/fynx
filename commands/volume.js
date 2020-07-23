0afbf45cecc0f8fb48428122aed780fb3ac4de0e
Glitch (harmonymuzik) <none>
2020-07-23 12:49:27 +0000

    🚶🍖 Checkpoint ./commands/volume.js:2344545/651 ./package.json:2344545/1610

--- commands/volume.js
+++ commands/volume.js
@@ -3,16 +3,17 @@ const fs = require("fs")
 
 module.exports.run = async (client, message, args) => {
 
-  if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `<a:yanlis:734892943332212764>  | Ses seviyesini ayarlabilmek için bir ses kanalında olmanız gerekmektedir!` }})
+  if(!message.member.voice.channel) return message.channel.send({embed: {color: "RED", description: `<a:yanlis:734892943332212764>  | Ses seviyesini ayarlabilmek için bir ses kanalında olmanız gerekmektedir!` }})
     
-  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `<a:yanlis:734892943332212764>  | Şu anda hiçbir müzik çalmamaktadır!` }})
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: "RED", description: `<a:yanlis:734892943332212764>  | Şu anda hiçbir müzik çalmamaktadır!` }})
   let volume = parseInt(args.join(" "));
-  if (!volume) return message.channel.send({embed: {color: client.colors.error, description: `<a:yanlis:734892943332212764>  | Ses seviyesini ayarlayabilmek için sadece bir doğal sayı giriniz.` }})
-  if (isNaN(args[0])) return message.channel.send({embed: {color: client.colors.error, description: `<a:yanlis:734892943332212764>  | Ses seviyesini ayarlayabilmek için sadece bir doğal sayı giriniz.` }})
-  
+  if (!volume) return message.channel.send({embed: {color: "RED", description: `<a:yanlis:734892943332212764>  | Ses seviyesini ayarlayabilmek için sadece bir doğal sayı giriniz.` }})
+  if (isNaN(args[0])) return message.channel.send({embed: {color: "RED", description: `<a:yanlis:734892943332212764>  | Ses seviyesini ayarlayabilmek için sadece bir doğal sayı giriniz.` }})
+  if (args > 100) return message.channel.send({embed: {color: "RED", description: `Ses seviyesini sadece \`0\` ile \`100\` arasında ayarlayabilirsiniz. `}})
+  if (args < 0) return message.channel.send({embed: {color: "RED", description: `Ses seviyesini sadece \`0\` ile \`100\` arasında ayarlayabilirsiniz. ` }})
   client.player.setVolume(message.guild.id, volume);
     
-  message.channel.send({embed: {color: client.colors.success, description: `<a:tik:734892939737694239>  | Ses seviyesi, başarılı bir şekilde \`${args.join(" ")}\` olarak ayarlandı! ` }})
+  message.channel.send({embed: {color: "GREEN", description: `<a:tik:734892939737694239>  | Ses seviyesi, başarılı bir şekilde \`${args.join(" ")}\` olarak ayarlandı! ` }})
 
 
 }
@@ -20,4 +21,4 @@ module.exports.run = async (client, message, args) => {
 module.exports.config = {
   name: "set-volume",
   aliases: ['sv']
-}
+};

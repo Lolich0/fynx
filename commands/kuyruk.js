07ff0fd7430f639b6d7dacc932d25a89cbefbf61
Glitch (lavender-fifth-gander) <none>
2020-07-23 21:42:28 +0000

    🐩🛫 Checkpoint ./commands/kuyruk.js:2344545/22 ./commands/oynat.js:2344545/3445

--- commands/kuyruk.js
+++ commands/kuyruk.js
@@ -16,7 +16,7 @@ module.exports.run = async (client, message) => {
     let q = queue.songs.map((song, i) => {
         return `${i === 0 ? '<a:calan:735111831550427166>  | Şu Anda Çalınan Müzik' : `${i+1}`} \n**Müzik:** \`${song.name}\` \n**Kanal:** \`${song.author}\`\n**Süre:** \`${song.duration}\``
     }).join('\n');  
-       message.channel.send({embed: {color: embedSuccess, description: `${q}`, thumbnail: `${thm}` }})
+       message.channel.send({embed: {color: embedSuccess, description: `${q}`}})
 };
 
   

90e6f6baa48f203989c9d52e4fcba2c7c2a5a342
Glitch (lavender-fifth-gander) <none>
2020-07-25 08:47:49 +0000

    🏰👀 Checkpoint ./commands/davet.js:2344545/1189

---
+++ commands/davet.js
@@ -0,0 +1,25 @@
+const Discord = require ('discord.js');
+const db = require ('quick.db')
+
+exports.run = async (client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+ .setColor("BLUE")
+    .setAuthor(`Harmony Bot Linkler`, client.user.avatarURL())
+ .setDescription('<:site:735595781226102795>  | [Harmony Bot İnternet Sayfası](https://harmonybot.tk/)\n\n<:bot:734888733853089903>  | [Harmony Bot`u Sunucunuza Ekleyin!](https://discordapp.com/oauth2/authorize?client_id=511593657711722523&scope=bot&permissions=8)')
+.setFooter(`${message.author.username} tarafından istendi!`) 
+.setTimestamp()
+        .setThumbnail('https://cdn.discordapp.com/attachments/703582257570644029/734646429833756732/HarmonyMusic.png')
+    message.channel.send(embed)
+   
+ };
+ exports.config = {
+   enabled: true,
+   guildOnly: false,
+   aliases: ["invitation","site"],
+   permLevel: 0
+ };
+ exports.help = {
+   name: 'davet',
+   description: 'Botun davet bağlantılarını gösterir',
+   usage: 'davet'
+ };

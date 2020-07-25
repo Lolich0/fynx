be03e997792071c7de5a265542f0c0fa1b7bccef
Glitch (lavender-fifth-gander) <none>
2020-07-25 08:59:13 +0000

    🐝👲 Checkpoint ./commands/istatistik.js:2344545/569

--- commands/istatistik.js
+++ commands/istatistik.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, args) => {
     .format(" D [gün], H [saat], m [dakika], s [saniye]");
   const istatistikler = new Discord.MessageEmbed()
     .setColor("RANDOM")
-    .setTimestamp()
+    .setTimestamp()//client.guilds.cache.size //client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()
     .setFooter("Harmony © 2020", client.user.avatarURL())
     .addField("<a:coder:734885134800519288>  | **Botun Sahibi**", "<@327064201245753344>")
     .addField("<:ping:735102803340558336>  | **Gecikme süreleri**","Mesaj Gecikmesi: {ping1} ms \nBot Gecikmesi: {ping2} ms"
@@ -18,7 +18,10 @@ exports.run = async (client, message, args) => {
     .addField("<:uptime:735102804468957284>  | **Çalışma süresi**", seksizaman, true)
     .addField("<:discordjs:735102801449058364>  | **Discord.JS sürüm**", "v" + Discord.version, true)
     .addField("<:nodejs:735102802614943826>  | **Node.JS sürüm**", `${process.version}`, true)
-    .addField(":microphone:  | **Müzik Çalınan Sunucu Sayısı**", client.voice.connections.size, true)
+    .addField(":microphone:  | **Müzik Oynatılan Sunucu Sayısı**", client.voice.connections.size, true)
+  .addField(`<:Belge:735061681197285476>  | **Sunucu Sayısı**`, client.guilds.cache.size, true)
+.addField(`:family_man_girl:  | **Kullanıcı Sayısı**`, client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
+  .addField(`<:sunucu:736507254857269328>  | **Sunucu Lokasyonu**`, `<a:turkiye:734888652827656262>  Turkey, Izmir`, true)
   return message.channel.send(istatistikler);
 };
 
